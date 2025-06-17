21bef300facdc8e3988d42af143539c6072fe33a
Jefferson B. Alexandrino <jeffalexandrino07@gmail.com>
2025-06-17 20:13:52 -0300

    att

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -1,49 +1,32 @@
-import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
-import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 import http from 'k6/http';
-import { check } from 'k6';
+import { sleep } from 'k6';
 import { Trend, Rate } from 'k6/metrics';
 
-export const getContactsDuration = new Trend('get_contacts', true);
-export const RateContentOK = new Rate('content_OK');
+const getDuration = new Trend('get_duration');
+const successRate = new Rate('success_rate');
 
-export const options = {
-  thresholds: {
-    http_req_failed: ['rate<0.30'],
-    get_contacts: ['p(99)<500'],
-    content_OK: ['rate>0.95']
-  },
+export let options = {
   stages: [
-    { duration: '10s', target: 5 },
-    { duration: '20s', target: 15 }
-  ]
+    { duration: '30s', target: 10 },
+    { duration: '1m', target: 50 },
+    { duration: '1m', target: 100 },
+    { duration: '1m', target: 200 },
+    { duration: '30s', target: 300 }
+  ],
+  thresholds: {
+    http_req_duration: ['p(95)<5700'],
+    http_req_failed: ['rate<0.12']
+  }
 };
 
-export function handleSummary(data) {
-  return {
-    './src/output/index.html': htmlReport(data),
-    stdout: textSummary(data, { indent: ' ', enableColors: true })
-  };
-}
-
 export default function () {
-  const baseUrl = 'https://test.k6.io/';
+  // Endpoint da Wikipedia
+  const res = http.get(
+    'https://en.wikipedia.org/api/rest_v1/page/random/summary'
+  );
 
-  const params = {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
+  getDuration.add(res.timings.duration);
+  successRate.add(res.status === 200);
 
-  const OK = 200;
-
-  const res = http.get(`${baseUrl}`, params);
-
-  getContactsDuration.add(res.timings.duration);
-
-  RateContentOK.add(res.status === OK);
-
-  check(res, {
-    'GET Contacts - Status 200': () => res.status === OK
-  });
+  sleep(1);
 }
