f01a28a60e2e66f643aec0944a795c81025eadfd
Jefferson B. Alexandrino <jeffalexandrino07@gmail.com>
2025-06-17 20:23:09 -0300

    att

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -7,11 +7,10 @@ const successRate = new Rate('success_rate');
 
 export let options = {
   stages: [
-    { duration: '30s', target: 10 },
-    { duration: '1m', target: 50 },
+    { duration: '1m30s', target: 10 },
+    { duration: '1m30s', target: 100 },
     { duration: '1m', target: 100 },
-    { duration: '1m', target: 200 },
-    { duration: '30s', target: 300 }
+    { duration: '1m', target: 300 }
   ],
   thresholds: {
     http_req_duration: ['p(95)<5700'],
@@ -20,9 +19,14 @@ export let options = {
 };
 
 export default function () {
-  // Endpoint da Wikipedia
   const res = http.get(
-    'https://en.wikipedia.org/api/rest_v1/page/random/summary'
+    'https://en.wikipedia.org/api/rest_v1/page/random/summary',
+    {
+      headers: {
+        'User-Agent': 'k6-loadtest-agent/1.0',
+        Accept: 'application/json'
+      }
+    }
   );
 
   getDuration.add(res.timings.duration);
